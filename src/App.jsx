b334a2c4a4144a5ff678516b757bc22e9b6b34d1
Diego Viana <diegovianaf7@gmail.com>
2022-10-04 13:47:06 -0300

    add todo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
 function App() {
+  const [inputTask, setInputTask] = useState()
+  
+  const tasks = useSelector((state) => state.tasks)
+  const dispatch = useDispatch()
+
+  const handleInput = (e) => {
+    setInputTask(e.target.value)
+  }
+
+  const handleSubmit = () => {
+    dispatch({
+      type: 'ADD_TASK',
+      payload: inputTask,
+    })
+  }
+
   return (
-    <h1>
-      Hallo Welt
-    </h1>
+    <>
+      <ul>
+        { tasks.map((task) => <li key={task}>{task}</li>) }
+      </ul>
+      <input type="text" onChange={handleInput} value={inputTask} />
+      <button onClick={handleSubmit}>Add Task</button>
+    </>
   )
 }
 
